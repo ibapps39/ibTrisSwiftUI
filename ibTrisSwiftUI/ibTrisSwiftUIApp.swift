a07ca4d30b8c720f3a13d57d9aaa7060ef9a1e05
Ian Brown <ibapps39@gmail.com>
2024-09-23 09:14:39 -0600

    Initial Commit

---
+++ ibTrisSwiftUI/ibTrisSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ibTrisSwiftUIApp.swift
+//  ibTrisSwiftUI
+//
+//  Created by Ian Brown on 9/23/24.
+//
+
+import SwiftUI
+
+@main
+struct ibTrisSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
